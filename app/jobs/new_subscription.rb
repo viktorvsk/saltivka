01bb7466c2824ac9b20c8cb005dfc9728658783f
Viktor Vsk <me@viktorvsk.com>
2023-06-19 11:23:16 +0200

    Rename to pubsub_id

--- app/jobs/new_subscription.rb
+++ app/jobs/new_subscription.rb
@@ -4,15 +4,16 @@ class NewSubscription
 
   def perform(connection_id, subscription_id, filters)
     filters = JSON.parse(filters)
+    pubsub_id = "#{connection_id}:#{subscription_id}"
 
     union = filters.map { |filter_set| Event.by_nostr_filters(filter_set).to_sql }.join("\nUNION\n")
 
     ids = Event.find_by_sql(union).pluck(:id)
 
     Event.where(id: ids).find_each do |event|
-      REDIS.publish("events:#{connection_id}:#{subscription_id}", event.to_json)
+      REDIS.publish("events:#{pubsub_id}", event.to_json)
     end
 
-    REDIS.publish("events:#{connection_id}:#{subscription_id}", "EOSE")
+    REDIS.publish("events:#{pubsub_id}", "EOSE")
   end
 end

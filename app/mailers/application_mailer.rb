d68d1bd5e66365797c89e58f001ae404b9aeb4a0
Viktor Vsk <me@viktorvsk.com>
2023-06-30 16:23:22 +0200

    Adjust default mailer and test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: RELAY_CONFIG.mailer_default_from
   layout "mailer"
 end
--- test/mailers/mailer_test.rb
+++ test/mailers/mailer_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class MailerTest < ActiveSupport::TestCase
   test "Has configurable default from" do
     RELAY_CONFIG.stub(:mailer_default_from, "admin@nostr.com") do
-      assert "admin@nostr.com", ApplicationMailer.default[:from]
+      assert_equal "admin@nostr.com", ApplicationMailer.default[:from]
     end
   end
 end

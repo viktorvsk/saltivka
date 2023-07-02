9c05728e752c96795b9e7a79b8165014f3fe2ac5
Viktor Vsk <me@viktorvsk.com>
2023-07-02 03:31:45 +0200

    Rebrand

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Nostrails
+module Saltivka
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0

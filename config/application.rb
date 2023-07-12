430999d8cbb3475370551957b12933ae91a3747f
Viktor Vsk <me@viktorvsk.com>
2023-07-13 01:44:42 +0200

    Set application time to UTC

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module Saltivka
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = "UTC"
+
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end

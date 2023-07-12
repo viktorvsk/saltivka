427fd629f0e958dc9d9b02b8a18467a9952921d3
Viktor Vsk <me@viktorvsk.com>
2023-07-13 01:57:03 +0200

    AuthorizeRequest results to auth_level=2 if active AuthorSubscription was found

--- app/jobs/authorization_request.rb
+++ app/jobs/authorization_request.rb
@@ -6,15 +6,15 @@ class AuthorizationRequest
   # i.e. when TrustedAuthor record is created check if there are
   # existing connections with this pubkey and update their auth_level
   # and do the opposite if TrustedAuthor record was deleted
+  # NOTE: it seems it makes sense to notify client AUTH is succesful, but NIP-16/NIP-20 tell us not to fanout ephemeral events
+  # MemStore.fanout(cid: connection_id, command: :ok, payload: ["OK", event_sha256, true, ""].to_json)
   def perform(connection_id, event_sha256, pubkey)
     if TrustedAuthor.joins(:author).where(authors: {pubkey: pubkey}).exists?
       MemStore.authorize!(cid: connection_id, level: "4")
-
-    # It seems it makes sense to notify client AUTH is succesful, but NIP-16/NIP-20 tell us not to fanout ephemeral events
-    # MemStore.fanout(cid: connection_id, command: :ok, payload: ["OK", event_sha256, true, ""].to_json)
-    # elsif assign level 3 if user has paid subscription
-    # elsif assign level 2 if user is registered
-    # elsif assign level -1 if user is banned
+    elsif AuthorSubscription.joins(:author).where(authors: {pubkey: pubkey}).active.exists?
+      MemStore.authorize!(cid: connection_id, level: "3")
+    # elsif User.joins(:authors).where(authors: {pubkey: pubkey}).active.exists?
+    #   MemStore.authorize!(cid: connection_id, level: "2")
     else
       # assign level 1 if user is a guest but with authenticated public key
       MemStore.fanout(cid: connection_id, command: :ok, payload: ["OK", event_sha256, false, "restricted: unknown author"].to_json)

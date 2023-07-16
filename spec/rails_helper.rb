cd4de68be09c9efc508756d0c4b1dff73b601085
Viktor Vsk <me@viktorvsk.com>
2023-07-16 10:17:03 +0200

    Improved rails c -e test usage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,8 @@ SimpleCov.start("rails") do
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "spec_helper"
+require "rspec/core"
+require_relative "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production

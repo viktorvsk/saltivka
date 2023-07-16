ae57d47921e1b3d235904952b077dc12ecc6b177
Viktor Vsk <me@viktorvsk.com>
2023-07-16 21:46:36 +0200

    Fix validation for User and DeleteEvent

--- app/models/delete_event.rb
+++ app/models/delete_event.rb
@@ -1,6 +1,6 @@
 class DeleteEvent < ApplicationRecord
   belongs_to :author
-  validates :sha256, uniqueness: {scope: [:author]}, length: {is: 64}
+  validates :sha256, uniqueness: {scope: [:author]}, length: {is: 64}, presence: true
 
   scope :by_pubkey_and_sha256, ->(pubkey, sha256) { joins(:author).where(authors: {pubkey: pubkey}, sha256: sha256) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 
   scope :active, -> { where.not(confirmed_at: nil) }
 

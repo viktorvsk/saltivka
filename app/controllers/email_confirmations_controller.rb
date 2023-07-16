093b38a8db3771f479b3af4d81413ffcad27b388
Viktor Vsk <me@viktorvsk.com>
2023-07-16 12:24:00 +0200

    Controller to confirm email through email link

---
+++ app/controllers/email_confirmations_controller.rb
@@ -0,0 +1,17 @@
+class EmailConfirmationsController < ApplicationController
+  def show
+    token = params[:id]
+    email = MemStore.find_email_to_confirm(token)
+
+    if email && (User.where(email: email, confirmed_at: nil).update_all(confirmed_at: Time.current) == 1)
+      MemStore.confirm_email(token)
+      user = User.find_by_email(email)
+      auto_login(user)
+      flash[:notice] = "Email was successfully confired!"
+    else
+      flash[:alert] = "Email was not confirmed: confirmation token not found or expired"
+    end
+
+    redirect_to root_path
+  end
+end

ecb56f642c2685b67cd4c019f2eae3c981f7d062
Viktor Vsk <me@viktorvsk.com>
2023-07-29 20:46:17 +0200

    Properly handle manual client disconnect from admin

--- app/controllers/admin/connections_controller.rb
+++ app/controllers/admin/connections_controller.rb
@@ -4,7 +4,12 @@ class Admin::ConnectionsController < AdminController
   end
 
   def destroy
-    MemStore.fanout(cid: params[:id], command: :terminate, payload: [3502, "restricted: connection was closed by the server"].to_json)
+    subscribers_count = MemStore.fanout(cid: params[:id], command: :terminate, payload: [3502, "restricted: connection was closed by the server"].to_json)
+
+    if subscribers_count.zero?
+      # It means that client was already disconnected and resource cleanup wasn't handled correctly
+      Sidekiq.redis { |c| Nostr::RelayController.new(connection_id: params[:id], rate_limited: false).terminate(event: nil, redis: c) }
+    end
 
     redirect_to admin_connections_path
   end

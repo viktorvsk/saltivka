14549ce02542cf05829b4d1f21c2a1d3eaeefdde
Viktor Vsk <me@viktorvsk.com>
2023-08-20 14:47:45 +0200

    Allow building Docker image without a running Redis service

--- config/initializers/redis_indexes.rb
+++ config/initializers/redis_indexes.rb
@@ -1,12 +1,14 @@
-begin
-  Sidekiq.redis do |c|
-    c.pipelined do
-      c.select("0")
-      c.call(RedisSearchCommands::CREATE_SCHEMA_COMMAND.split(" "))
+if ENV["BUILD_STEP"].blank?
+  begin
+    Sidekiq.redis do |c|
+      c.pipelined do
+        c.select("0")
+        c.call(RedisSearchCommands::CREATE_SCHEMA_COMMAND.split(" "))
+      end
     end
-  end
-rescue RedisClient::CommandError => e
-  if e.message != "Index already exists"
-    raise(e)
+  rescue RedisClient::CommandError => e
+    if e.message != "Index already exists"
+      raise(e)
+    end
   end
 end

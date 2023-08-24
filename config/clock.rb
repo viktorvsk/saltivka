8f4edfdd978544d945c4de9f8d41fbd65b543a14
Viktor Vsk <me@viktorvsk.com>
2023-08-24 11:56:35 +0200

    Move clock.rb to config

--- config/clock.rb
+++ config/clock.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require "clockwork"
-require './config/boot'
-require './config/environment'
+require "./config/boot"
+require "./config/environment"
 
 module Clockwork
   every(30.minutes, "Cleanup connections data", skip_first_run: true) { CleanupConnections.perform_later }

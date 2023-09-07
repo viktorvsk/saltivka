1c5993e8e5974ff119a5a29b49dd0e168f6b7a43
Viktor Vsk <me@viktorvsk.com>
2023-09-07 20:58:40 +0200

    Decrease number of samples sent to sentry performance wise

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,7 +8,7 @@ if ENV["SENTRY_DSN"].present?
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
+    config.traces_sample_rate = 0.05
     # or
     config.traces_sampler = lambda do |_context|
       true

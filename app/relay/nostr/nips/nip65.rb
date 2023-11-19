8e905e94b7ff47f0876f87a7e521ba6a0e5684fd
Viktor Vsk <me@viktorvsk.com>
2023-11-19 16:49:59 +0100

    Refactor event kind exepmtion (NIP-65) to work for COUNT command

--- app/relay/nostr/nips/nip65.rb
+++ app/relay/nostr/nips/nip65.rb
@@ -2,15 +2,16 @@ module Nostr
   module Nips
     module Nip65
       def self.call(command, nostr_event)
-        if command === "EVENT"
+        case command
+        when "EVENT"
           event = nostr_event.last
-          return true if event["kind"].to_i.in?(RELAY_CONFIG.kinds_exempt_of_auth)
-        elsif command === "REQ"
+          true if event["kind"].to_i.in?(RELAY_CONFIG.kinds_exempt_of_auth)
+        when "REQ", "COUNT"
           filters = nostr_event[1..]
-          return true if filters&.all? { |f| f["kinds"]&.all? { |k| k.to_i.in?(RELAY_CONFIG.kinds_exempt_of_auth) } }
+          true if filters&.all? { |f| f["kinds"]&.all? { |k| k.to_i.in?(RELAY_CONFIG.kinds_exempt_of_auth) } }
+        else
+          false
         end
-
-        false
       end
     end
   end
